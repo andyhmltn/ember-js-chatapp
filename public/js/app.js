c88febd60bffbe2dacb620ae18f031546f6acad5
Declan de Wet <declandewet@me.com>
2013-02-18 06:04:01 +0200

    JSLint compliance. This is a breaking change, but quality code first, right?

--- public/js/app.js
+++ public/js/app.js
@@ -1,131 +1,125 @@
 $(function() {
-	Chat = Ember.Application.create({
-		rootElement: '#wrapper',
-		socket: new io.connect(window.location.hostname),
-		currentUser: null,
-		nameEntered: false
-	});
+  "use strict";
+  var Chat;
+  var socket = io.connect(window.location.hostname);
+  Chat = Ember.Application.create({
+    rootElement: '#wrapper',
+    socket: socket,
+    currentUser: null,
+    nameEntered: false
+  });
 
-	Chat.scroll_to_bottom = function() { 
-		setTimeout(function(){
-		  $(".chatlist").scrollTop($(".chatlist")[0].scrollHeight);
-		}, 500);
-	}
+  Chat.scrollToBottom = function() { 
+    setTimeout(function(){
+      $(".chatlist").scrollTop($(".chatlist")[0].scrollHeight);
+    }, 500);
+  };
 
-	Chat.Message = Ember.Object.extend({
-		message: null,
-		name: null
-	});
+  Chat.Message = Ember.Object.extend({
+    message: null,
+    name: null
+  });
 
-	Chat.User = Ember.Object.extend({
-		name: null
-	});
+  Chat.User = Ember.Object.extend({
+    name: null
+  });
 
-	Chat.MessageController = Ember.Controller.extend({
-		messages: Ember.A(),
+  Chat.MessageController = Ember.Controller.extend({
+    messages: Ember.A(),
 
-		init: function() {
-			stored_name = localStorage.getItem('chatapp:username') 
-			if(stored_name!= null) {
-				Chat.set('currentUser', stored_name);
-				Chat.set('nameEntered', true);
+    init: function() {},
 
-				Chat.socket.emit('join', stored_name);
-			}
-		},
-		
-		createMessage: function(message, user)
-		{
-			this.get('messages').addObject(Chat.Message.create({message:message, name:user}));
-		}	
-	});
+    createMessage: function(message, user) {
+      this.get('messages').addObject(Chat.Message.create({message:message, name:user}));
+    }	
+  });
 
-	Chat.UserController = Ember.Controller.extend({
-		users: Ember.A(),
+  Chat.UserController = Ember.Controller.extend({
+    users: Ember.A(),
 
-		init: function() {
-			stored_name = localStorage.getItem('chatapp:username') 
-			if(stored_name!= null) {
-				Chat.set('currentUser', stored_name);
-				Chat.set('nameEntered', true);
+    init: function() {
+      var storedName = localStorage.getItem('chatapp:username');
+      if(storedName !== null) {
+        Chat.set('currentUser', storedName);
+        Chat.set('nameEntered', true);
 
-				Chat.socket.emit('join', stored_name);
-			}
-		},
+        Chat.socket.emit('join', storedName);
+      }
+    },
 
-		createUser: function(name)
-		{
-			this.get('users').addObject(Chat.User.create({name:name}));
-		},
+    createUser: function(name) {
+      this.get('users').addObject(Chat.User.create({name:name}));
+    },
 
-		removeUser: function(property, value)
-		{
-			object = this.findProperty(property, value);
+    removeUser: function(property, value) {
+      var object = this.findProperty(property, value);
+      this.removeObject(object);
+    }
+  });
 
-			this.removeObject(object);
-		}
-	});
+  Chat.messageController = Chat.MessageController.create();
+  Chat.userController = Chat.UserController.create();
 
-	Chat.messageController = Chat.MessageController.create();
-	Chat.userController = Chat.UserController.create();
+  Chat.EnterNameView = Ember.TextArea.extend({
+    insertNewline: function(e) {
+      e.preventDefault();
+      var value = this.get('value');
 
-	Chat.EnterNameView = Ember.TextArea.extend({
-		insertNewline: function(e) {
-			var value = this.get('value');
+      if (value !== null) {
+        this.set('value', null);
 
-			if(value != null) {
-				this.set('value', null);
+        Chat.socket.emit('join', value);
+        Chat.set('nameEntered', true);
 
-				Chat.socket.emit('join', value);
-				Chat.set('nameEntered', true);
+        localStorage.setItem('chatapp:username', value);
+      }
+    }
+  });
 
-				localStorage.setItem('chatapp:username', value);
-			}
-		}
-	});
+  Chat.CreateMessageView = Ember.TextArea.extend({
+    insertNewline: function() {
+      var value = this.get('value');
 
-	Chat.CreateMessageView = Ember.TextArea.extend({
-		insertNewline: function() {
-			var value = this.get('value');
+      if (value !== null) {
+        this.set('value', null);
 
-			if (value != null) {
-				this.set('value', null);
+        var user = Chat.currentuser;
 
-				var user = Chat.currentuser;
+        if (user !== null) {
+          Chat.messageController.createMessage(value, user);
+        }
+        Chat.socket.emit('messages', value);
+      }
+    }
+  });
 
-				if(user != null) Chat.messageController.createMessage(value, user);
-				Chat.socket.emit('messages', value);
-			}
-		}
-	});
+  Chat.socket.on('messages', function(data) {
+    data = JSON.parse(data);
 
-	Chat.socket.on('messages', function(data) {
-		data = JSON.parse(data);
+    Chat.messageController.createMessage(data.message.replace("<br />","\n"), data.name);
 
-		Chat.messageController.createMessage(data.message.replace("<br />","\n"), data.name);
+    Chat.scroll_to_bottom();
+  });
 
-		Chat.scroll_to_bottom();
-	});
+  Chat.socket.on('chatters', function(data) {
+    data = JSON.parse(data);
 
-	Chat.socket.on('chatters', function(data) {
-		data = JSON.parse(data);
+    $.each(data, function(key, value) {
+      Chat.userController.createUser(value);
+    });
+  });
 
-		$.each(data, function(key, value) {
-			Chat.userController.createUser(value);
-		});
-	});
+  Chat.socket.on('remove chatter', function(name) {
+    Chat.userController.removeUser('name', name);
+  });
 
-	Chat.socket.on('remove chatter', function(name) {
-		Chat.userController.removeUser('name', name);
-	});
+  Chat.socket.on('add chatter', function(name) {
+    Chat.userController.createUser(name);
+  });
 
-	Chat.socket.on('add chatter', function(name) {
-		Chat.userController.createUser(name);
-	});
+  Chat.scroll_to_bottom();
 
-	Chat.scroll_to_bottom();
-
-	$(window).unload(function() {
-		Chat.socket.disconnect();
-	});
+  $(window).unload(function() {
+    Chat.socket.disconnect();
+  });
 });
